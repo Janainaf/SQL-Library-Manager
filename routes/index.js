1d7c16049643f1eefd7a685cee5995c3e8f217e3
Janainaf <figueira.janaina@gmail.com>
2022-02-10 19:14:13 +0100

    showing book titles

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const Book = require("../models").Book;
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  // res.render('index', { title: 'Express' });
-});
+/* Handler function to wrap each route. */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
+}
+
+/* GET articles listing. */
+router.get(
+  "/books",
+  asyncHandler(async (req, res) => {
+    const books = await Book.findAll({ order: [["createdAt", "DESC"]] });
+    res.render("index", { books, title: "Book list" });
+    console.log(books);
+  })
+);
 
 module.exports = router;
